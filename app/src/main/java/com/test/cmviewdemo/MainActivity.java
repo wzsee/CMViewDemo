010bf121ff0f84f47e53fc4c9a2bad26508342b1
hualianrensheng <chenmu889@163.com>
2018-01-22 17:42:38 +0800

    ColorTrackTextView 01

--- app/src/main/java/com/test/cmviewdemo/MainActivity.java
+++ app/src/main/java/com/test/cmviewdemo/MainActivity.java
@@ -4,11 +4,16 @@ import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private QQStyepView cmMainQQStep;
+    private ColorTrackTextView colorTrackTextView;
+    private Button leftToRight;
+    private Button rightToLeft;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +21,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initQQStep();
+
+        initColorTrackView();
+
+    }
+
+
+    private void initColorTrackView() {
+        colorTrackTextView = findViewById(R.id.main_TrackTextView);
+
+        leftToRight = findViewById(R.id.main_left_to_right);
+        rightToLeft = findViewById(R.id.main_right_to_left);
+
+        leftToRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+                valueAnimator.setDuration(2000);
+                valueAnimator.setInterpolator(new DecelerateInterpolator());
+
+                colorTrackTextView.setDirectory(ColorTrackTextView.Directory.LEFT_TO_RIGHT);
+
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float animatedValue = (float) animation.getAnimatedValue();
+                        colorTrackTextView.setCurrentProgress(animatedValue);
+                    }
+                });
+
+                valueAnimator.start();
+            }
+        });
+
+        rightToLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ValueAnimator valueAnimator = ObjectAnimator.ofFloat(0, 1);
+                valueAnimator.setDuration(2000);
+                valueAnimator.setInterpolator(new DecelerateInterpolator());
+
+                colorTrackTextView.setDirectory(ColorTrackTextView.Directory.RIGHT_TO_LEFT);
+
+                valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        float animatedValue = (float) animation.getAnimatedValue();
+                        colorTrackTextView.setCurrentProgress(animatedValue);
+                    }
+                });
+
+                valueAnimator.start();
+            }
+        });
     }
 
     private void initQQStep() {
-        cmMainQQStep = (QQStyepView)findViewById(R.id.main_qqstep);
+        cmMainQQStep = findViewById(R.id.main_qqstep);
         //1.设置最大步数
         cmMainQQStep.setMaxStep(4000);
         //2.添加属性动画
@@ -29,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                float animatedValue = (float)animation.getAnimatedValue();
+                float animatedValue = (float) animation.getAnimatedValue();
                 cmMainQQStep.setStepProgress((int) animatedValue);
             }
         });
